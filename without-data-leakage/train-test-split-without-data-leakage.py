214ce84c7ae90e64483c167ee5848b6af44a5fdc
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2022-12-28 22:19:50 +0700

    add train-test-split-without-data-leakage.py

---
+++ without-data-leakage/train-test-split-without-data-leakage.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+if __name__ == '__main__':
+    X, y = make_classification(n_samples=1000, n_features=20, n_informative=15, n_redundant=5, random_state=7)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+    scaler = MinMaxScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    scaler.fit(X_test)
+    X_test = scaler.transform(X_test)
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    yhat = model.predict(X_test)
+    accuracy = accuracy_score(y_test, yhat)
+    print(f'Accuracy {accuracy * 100}')
