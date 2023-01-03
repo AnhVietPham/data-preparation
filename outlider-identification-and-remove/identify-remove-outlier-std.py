a0b17dad69e1f67f7850388c692a84133c57a3cd
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2023-01-03 21:10:18 +0700

    outlier: identify-remove-outlier-std.py

--- outlider-identification-and-remove/identify-remove-outlier-std.py
+++ outlider-identification-and-remove/identify-remove-outlier-std.py
@@ -1,14 +1,16 @@
 from numpy.random import seed
-from numpy.random import rand
+from numpy.random import randn
 from numpy import mean
 from numpy import std
 
 seed(1)
 
 if __name__ == "__main__":
-    data = 5 * rand(10000) + 50
+    data = 5 * randn(10000) + 50
     data_mean, data_std = mean(data), std(data)
     cut_off = data_std * 3
     lower, upper = data_mean - cut_off, data_mean + cut_off
     outliers = [x for x in data if x < lower or x > upper]
     print(f'Identified outliers: {len(outliers)}')
+    outliers_removed = [x for x in data if x >= lower and x <= upper]
+    print(f'Non-outlier observations: {len(outliers_removed)}')
