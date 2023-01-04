a8f037d3be4d03f417ab4ebda5835fa0fc2741e5
vietpa <pham.anh.viet.vlth@gmail.com>
2023-01-04 16:48:37 +0700

    load-summarize-dataset-missing-value.py

---
+++ use-statistical-imputation/load-summarize-dataset-missing-value.py
@@ -0,0 +1,9 @@
+from pandas import read_csv
+
+if __name__ == "__main__":
+    df = read_csv("../use-statistical-imputation/data/horse-colic.csv", header=None, na_values='?')
+    print(df.head())
+    for i in range(df.shape[1]):
+        n_miss = df[[i]].isnull().sum()
+        perc = n_miss / df.shape[0] * 100
+        print('> %d, Missing: %d (%.1f%%)' % (i, n_miss, perc))
