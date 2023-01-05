85ab39252f22cf1b2dfbf13f89b14bc633c2349d
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2023-01-05 23:10:19 +0700

    add imputing-missing-value-using-simpleimputer.py

--- use-statistical-imputation/imputing-missing-value-using-simpleimputer.py
+++ use-statistical-imputation/imputing-missing-value-using-simpleimputer.py
@@ -3,6 +3,15 @@ from pandas import read_csv
 from sklearn.impute import SimpleImputer
 
 if __name__ == "__main__":
-    df = read_csv("../use-statistical-imputation/data/horse-colic.csv")
+    df = read_csv("../use-statistical-imputation/data/horse-colic.csv", header=None, na_values='?')
     data = df.values
     ix = [i for i in range(data.shape[1]) if i != 23]
+    X, y = data[:, ix], data[:, 23]
+    print(f'Missing: {sum(isnan(X).flatten())}')
+    print(X)
+    imputer = SimpleImputer(strategy='mean')
+    imputer.fit(X)
+    Xtrans = imputer.transform(X)
+    print("=" * 50)
+    print(f'Missing: {sum(isnan(Xtrans).flatten())}')
+    print(Xtrans)
