d8438803d9a3e891224e79dbd1805103b70fd677
vietpa <pham.anh.viet.vlth@gmail.com>
2023-01-09 17:13:30 +0700

    add use_iterative_imputer_to_evaluate_model.py

---
+++ use_iterative_imputation/use_iterative_imputer_to_evaluate_model.py
@@ -0,0 +1,21 @@
+from numpy import mean
+from numpy import std
+from pandas import read_csv
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.pipeline import Pipeline
+
+if __name__ == "__main__":
+    df = read_csv("../use-statistical-imputation/data/horse-colic.csv", header=None, na_values="?")
+    data = df.values
+    ix = [i for i in range(data.shape[1]) if i != 23]
+    X, y = data[:, ix], data[:, 23]
+    model = RandomForestClassifier()
+    imputer = IterativeImputer()
+    pipeline = Pipeline(steps=[('i', imputer), ('m', model)])
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+    scores = cross_val_score(pipeline, X, y, scoring='accuracy', n_jobs=-1)
+    print('Mean Accuracy: %.3f (%.3f)' % (mean(scores), std(scores)))
