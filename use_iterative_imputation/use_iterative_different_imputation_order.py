4d67c0c5c780e64eb25a51c4bb8786e71c174821
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2023-01-15 20:30:15 +0700

    add use_iterative_different_imputation_order.py

---
+++ use_iterative_imputation/use_iterative_different_imputation_order.py
@@ -0,0 +1,26 @@
+from numpy import mean
+from numpy import std
+from pandas import read_csv
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.pipeline import Pipeline
+from matplotlib import pyplot
+
+if __name__ == "__main__":
+    df = read_csv("../use-statistical-imputation/data/horse-colic.csv", header=None, na_values="?")
+    data = df.values
+    ix = [i for i in range(data.shape[1]) if i != 23]
+    X, y = data[:, ix], data[:, 23]
+    results = list()
+    strategies = ['ascending', 'descending', 'roman', 'arabic', 'random']
+    for s in strategies:
+        pipeline = Pipeline(steps=[('i', IterativeImputer(imputation_order=s)), ('m', RandomForestClassifier())])
+        cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+        scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+        results.append(scores)
+        print('>%s %.3f (%.3f)' % (s, mean(scores), std(scores)))
+    pyplot.boxplot(results, labels=strategies, showmeans=True)
+    pyplot.show()
