ade717152de5bc0d1292b42cd332d239099a84e7
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2023-01-18 21:53:54 +0700

    add applying-ANOVA-F-statistic-feature-selection.py

---
+++ select-numerical-input-features/applying-ANOVA-F-statistic-feature-selection.py
@@ -0,0 +1,31 @@
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+from matplotlib import pyplot
+
+
+def load_dataset(filename):
+    df = read_csv(filename, header=None)
+    data = df.values
+    X = data[:, :-1]
+    y = data[:, -1]
+    return X, y
+
+
+def select_features(X_train, y_train, X_test):
+    fs = SelectKBest(score_func=f_classif, k='all')
+    fs.fit(X_train, y_train)
+    X_train_fs = fs.transform(X_train)
+    X_test_fs = fs.transform(X_test)
+    return X_train_fs, X_test_fs, fs
+
+
+if __name__ == "__main__":
+    X, y = load_dataset("../select-numerical-input-features/data/pima-indians-diabetes.csv")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+    X_train_fs, X_test_fs, fs = select_features(X_train, y_train, X_test)
+    for i in range(len(fs.scores_)):
+        print("Feature %d: %f" % (i, fs.scores_[i]))
+    pyplot.bar([i for i in range(len(fs.scores_))], fs.scores_)
+    pyplot.show()
